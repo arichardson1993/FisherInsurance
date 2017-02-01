a2f5b91d5474bb2686db45a5d4473a6be4afa9f1
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2017-02-01 18:47:02 -0500

    Changing the paths to CustomerCareController

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,13 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 
+[Route("customer")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
         return Ok("This is the index of the HomeController");
     }
-    public IActionResult Claims()
+    [Route("fileclaim")]
+    public IActionResult NewClaim()
     {
         return Ok("This is the Claims controller for Customer Care");
+    }
+    [Route("myclaims")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("This is the ClaimHistory method in the CustomerCareController");
     }
 }
